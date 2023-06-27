ae8210d2132e4891fc533e3ce827955aab0e5249
P4bl1t0ck <130107066+P4bl1t0ck@users.noreply.github.com>
2023-06-27 15:05:41 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
-#include<stdio.h>
-#include"logica.h"
+#include <stdio.h>
 
 int main (int argc, char *argv[]) {
 
-    float A[4][2]={{3,-7},{1.4,9},{7.8,-3.2},{8,0}};
-    int I[4][4]={{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}};
-      float respuesta[2][4];
-
-    float T[2][4];
+    /*Archivos calse 27/06/2023*/
 
 
-    trapuesta(A);
-    multi(A,T,I);
- 
-    
- return 0;
-}
+    FILE *archivo;
+    char nombre[50]="Luis";
+     char apellido[50]="Perez";
+
+    archivo=fopen("nombre.txt",w+);////crea archivo w
+
+    char buffer[100];
+
+    fgets(buffer,100,archivo); /// extra una liena entera se guarda en un arrglo de tipo char
+    fputs("mensaje",archivo);//// entra cadenas
+    fprintf(archivo,"%s %s",nombre,apellido); /// Para imprimir de una carpeta en especifico 
+    fscanf(archivo,"%s %s",&nombre,&apellido);/// para escanear datos 
+
+    fclose(archivo);
+
+
+    return 0;
+}  
